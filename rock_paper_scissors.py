34a9ea5c3eee1c5e8ac9fa028d302c0963f9e31f
Stinne De Wolf <stinne.de.wolf@icloud.com>
2025-08-13 18:00:43 +0200

    add rock paper scissors game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,42 @@
+# Write code below 💖
+
+print("Welcome to rock, paper or scissors!")
+print("  1) 🪨")
+print("  2) 🗒️")
+print("  3) ✂️")
+player_action = int(input("Pick a number:"))
+
+import random
+computer_action = random.randint(1, 3)
+
+if player_action == 1:
+  print("You chose: Rock")
+elif player_action == 2:
+  print("You chose: Paper")
+else:
+  print("You chose: Scissors")
+
+if computer_action == 1:
+  print(f"The computer chose: Rock")
+elif computer_action == 2:
+  print("The computer chose: Paper")
+else:
+  print("The computer chose: Scissors")
+
+if computer_action == player_action:
+  print("It's a tie")
+elif computer_action == 3 and player_action == 1:
+  print("You win!")
+elif computer_action == 1 and player_action == 3:
+  print("You lose...")
+elif computer_action == 2 and player_action == 3:
+  print("You win!")
+elif computer_action == 3 and player_action == 2:
+  print("You lose...")
+elif computer_action == 1 and player_action == 2:
+  print("You win!")
+elif computer_action == 2 and player_action == 1:
+  print("You lose...")
+else:
+  print("There has been an error.")
+
